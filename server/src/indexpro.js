96fdee369b03b9963dbc0068c6a4660ebd00114f
Ritesh456789 <128074680+Ritesh456789@users.noreply.github.com>
2025-08-21 17:49:10 +0530

    Update indexpro.js

--- server/src/indexpro.js
+++ server/src/indexpro.js
@@ -178,6 +178,6 @@ app.post("/api/verify-otp", async (req, resp) => {
 
   
 
-app.listen(5000,()=>{
-    console.log("API listening on http://localhost:5000");
-})
+app.listen(PORT, () => {
+  console.log(`API listening on port ${PORT}`);
+});
